aaa18786bc387ebcfc25c0109dc4fa3fc1e98f67
Norman Huth <norman@huth.it>
2023-11-14 18:25:23 +0100

    feat: add scope filter for notifications

--- app/Jobs/NewReleaseJob.php
+++ app/Jobs/NewReleaseJob.php
@@ -12,6 +12,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Arr;
 
 class NewReleaseJob implements ShouldQueue, ShouldBeUnique
 {
@@ -48,9 +49,49 @@ class NewReleaseJob implements ShouldQueue, ShouldBeUnique
         $comparison = Nova::comparison($previousVersion, $this->release->version);
         $compareRoute = route('compare', [parseVersion($previousVersion), $this->release->version]);
 
-        Notification::whereActive(true)->each(function (Notification $notification) use ($comparison, $compareRoute, $previousVersion) {
-            $notification->notify(new NewReleaseNotification($this->release, $comparison['files'], $compareRoute, $previousVersion));
-        });
+        Notification::whereActive(true)
+            ->each(function (Notification $notification) use ($comparison, $compareRoute, $previousVersion) {
+                if ($this->shouldNotify($comparison['files'], $notification->scope)) {
+                    $notification->notify(
+                        new NewReleaseNotification(
+                            $this->release,
+                            $comparison['files'],
+                            $compareRoute,
+                            $previousVersion
+                        )
+                    );
+                }
+            });
+    }
+
+    /**
+     * Determine if notification should send.
+     *
+     * @param array        $files
+     * @param string|null  $scope
+     *
+     * @return bool
+     */
+    protected function shouldNotify(array $files, ?string $scope): bool
+    {
+        if (empty($scope)) {
+            return true;
+        }
+
+        $flattened = implode("\n", Arr::flatten($files));
+
+        $patterns = explode("\n", trim($scope));
+        foreach ($patterns as $pattern) {
+            if (str_contains($flattened, $pattern)) {
+                return true;
+            }
+            $pattern = str_replace('*', '.*', $pattern);
+            if (preg_match('#' . $pattern . '#', $flattened)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
